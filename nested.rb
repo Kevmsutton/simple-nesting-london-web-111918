486ed7070f7a2fbc1f01e6fa8e9280784340d0f7
Kevin Sutton <kevmsutton@gmail.com>
2018-11-15 11:04:26 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -62,10 +62,13 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-programmer_hash[:yukihiro_matsumoto] => {
+hopper[programmer_hash][:yukihiro_matsumoto] => {
    :known_for => "Ruby",
     :languages => ["LISP", "C"]
  }
+ 
+person["favorite_thing"] = "Books"
+person["favorite_thing"] #=> "Books"
 
 end
 
